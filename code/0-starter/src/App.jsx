f2ed38a2a431525031e298a67ff9a17cc6f8410b
Maksim <lissetathegreatsheluponeva@gmail.com>
2023-10-16 11:39:13 +0800

    first commit

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RecipeTitle from './RecipeTitle'
+import './index.css'
+
+function App() {
+    return (
+        <article>
+            <h1>Менеджер рецептов</h1>
+            <RecipeTitle />
+        </article>
+    )
+}
+
+export default App
+
---
+++ code/0-starter/src/RecipeTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RecipeTitle() {
+    const title = 'Картофельное пюре';
+    return (
+        <h2>{ title }</h2>
+    )
+};
+export default RecipeTitle;
